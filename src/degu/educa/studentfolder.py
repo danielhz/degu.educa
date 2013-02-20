7d5ed8e28e2b9deb1bb23f8cea2e637b9b108e2e
Daniel Hernández <daniel@degu.cl>
2013-02-19 23:16:04 -0300

    Fixing folders changing the prefix optilux by degu

--- src/degu/educa/studentfolder.py
+++ src/degu/educa/studentfolder.py
@@ -1,10 +1,28 @@
+# -*- coding: utf-8 -*-
+
 from five import grok
 from zope import schema
 from plone.directives import form
+
+from zope.interface import Invalid
+
+from plone.namedfile.interfaces import IImageScaleTraversable
+from plone.namedfile.field import NamedBlobImage
+from plone.app.textfield import RichText
+
+from degu.educa import DeguEducaMessageFactory as _
 
 class IStudentFolder(form.Schema):
     """A container for students
     """
 
-# Note that we use the standard folder_listing view for this type, so there
-# is no specific view here
+class View(grok.View):
+    """Default view (called "@@view"") for a contact.
+    
+    The associated template is found in student_templates/view.pt.
+    """
+    
+    grok.context(IStudentFolder)
+    grok.require('zope2.View')
+    grok.name('view')
+
